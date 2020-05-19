e9d0e62f63b5a95a9b67177fce7ba0a2ce6d0eda
jmaheshwari <Jhalak_Maheshwari@intuit.com>
2020-05-19 15:37:04 -0700

    adding back to go up and navigate button to navigate between fragments

---
+++ app/src/main/java/com/jm/animals/view/ListFragment.kt
@@ -0,0 +1,41 @@
+package com.jm.animals.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.Navigation
+
+import com.jm.animals.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        buttonDetail.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
